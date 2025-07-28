55984946f76afd883ed8c2b1007fcc4020962f80
John Patrick Villalon <jocvillalon@yahoo.com.ph>
2025-07-28 13:20:22 +0800

    Invoices: Finished create, edit, delete

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,14 @@ import { revalidatePath } from 'next/cache';
 import postgres from 'postgres';
 import { redirect } from 'next/navigation';
 
+// Steps for both Create Data and Edit Data
+// Extracting the data from formData.
+// Validating the types with Zod.
+// Converting the amount to cents.
+// Passing the variables to your SQL query.
+// Calling revalidatePath to clear the client cache and make a new server request.
+// Calling redirect to redirect the user to the invoice's page.
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
@@ -16,6 +24,8 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
@@ -34,4 +44,28 @@ export async function createInvoice(formData: FormData) {
   // important for renewing the cached content in the page
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
